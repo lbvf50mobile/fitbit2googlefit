0a310ec937927cdeddeb9b7a36e53a7daa56a610
Dmitry <lbvf50.mobile@gmail.com>
2018-01-15 16:25:39 +0700

    Put stat into the before

--- spec/csvreader_spec.rb
+++ spec/csvreader_spec.rb
@@ -4,16 +4,16 @@ require 'csvreader'
 describe "Csvreader" do
     before(:all) do
         @csv = Csvreader.new("csv/fitbit_data_test_short.csv")
+        @stat = @csv.stat
     end
     it "should open file and return 2 dimentional array" do
         
         expect(@csv.arr).to be_an_instance_of(Array)
     end
     it "should return stat answering on stat method" do
-        stat = @csv.stat
-        expect(stat).to be_an_instance_of(Hash)
-        expect(stat.keys.include?(:rows_amount)).to be true
-        expect(stat.keys.include?(:rows_zero_amount)).to be true
-        expect(stat.keys.include?(:rows_steps_amount)).to be true
+        expect(@stat).to be_an_instance_of(Hash)
+        expect(@stat.keys.include?(:rows_amount)).to be true
+        expect(@stat.keys.include?(:rows_zero_amount)).to be true
+        expect(@stat.keys.include?(:rows_steps_amount)).to be true
     end
 end
