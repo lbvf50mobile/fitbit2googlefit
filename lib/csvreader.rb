60a3df1d52e94479e6cd6cfcee611daf8a1d5767
Dmitry <lbvf50.mobile@gmail.com>
2018-01-15 16:07:30 +0700

    Add simple stat values

--- lib/csvreader.rb
+++ lib/csvreader.rb
@@ -1,6 +1,10 @@
+require "csv"
 class Csvreader
     attr_reader :arr
     def initialize(path)
-        @arr = []
+        @arr = CSV.read(path)
+    end
+    def stat
+        {rows_amount: 0, rows_zero_amount: 0, rows_steps_amount: 0}
     end
 end
--- spec/csvreader_spec.rb
+++ spec/csvreader_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 require 'csvreader'
 
 describe "Csvreader" do
+    before(:all) do
+        @csv = Csvreader.new("fitbit_data.csv")
+    end
     it "should open file and return 2 dimentional array" do
-        csv = Csvreader.new("fitbit_data.csv")
-        expect(csv.arr).to be_an_instance_of(Array)
+        
+        expect(@csv.arr).to be_an_instance_of(Array)
+    end
+    it "should return stat answering on stat method" do
+        stat = @csv.stat
+        expect(stat).to be_an_instance_of(Hash)
+        expect(stat.keys.include?(:rows_amount)).to be true
+        expect(stat.keys.include?(:rows_zero_amount)).to be true
+        expect(stat.keys.include?(:rows_steps_amount)).to be true
     end
 end
