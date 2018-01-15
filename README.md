035b534932a65a6bc38a82b2beb925519662cfb0
Dmitry <lbvf50.mobile@gmail.com>
2018-01-15 18:57:16 +0700

    Caltulate amount of rows queue that dose not interupt by 'zero' row

--- README.md
+++ README.md
@@ -8,12 +8,15 @@ https://developers.google.com/fit/rest/
 ```
 "Flie name csv/fitbit_data.csv"
 "Read file:"
- 17.500000   0.625000  18.125000 ( 18.290494)
+ 17.547000   0.687000  18.234000 ( 18.284816)
 "Get stat:"
-  0.984000   0.015000   0.999000 (  1.007495)
+  1.047000   0.000000   1.047000 (  1.045038)
+"Select chnunks where we have queue not interapted by zero rows:"
+  0.891000   0.000000   0.891000 (  0.894764)
 "Total rows: 1009440"
 "Zero rows: 921454, 91%"
 "Steps rows: 87986, 8%"
+"Amount of filled chunks: 32494"
 ```
 
 Only 8% of rows have a step data;
--- stat.rb
+++ stat.rb
@@ -10,7 +10,13 @@ p "Read file:"
 puts Benchmark.measure {csv = Csvreader.new(file_path)}
 p "Get stat:"
 puts Benchmark.measure {stat = csv.stat}
+p "Select chnunks where we have queue not interapted by zero rows:"
+a = csv.arr[1..-1] # Cunt the title
+puts Benchmark.measure {
+    a = a.chunk{|n| !n[2].to_i.eql?(0)}.select{|n| true == n[0]}
+}
 
 p "Total rows: %s" % stat[:rows_amount]
 p "Zero rows: %d, %d%" % [stat[:rows_zero_amount], (stat[:rows_zero_amount]/stat[:rows_amount].to_f)*100]
 p "Steps rows: %d, %d%" % [stat[:rows_steps_amount], (stat[:rows_steps_amount]/stat[:rows_amount].to_f)*100]
+p "Amount of filled chunks: #{a.size}"
