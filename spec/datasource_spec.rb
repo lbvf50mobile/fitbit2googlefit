8854201ae7748fbbff3715799b8e28b446c5a464
Dima <lbvf50.mobile@gmail.com>
2018-01-21 07:28:31 -0800

    Add Random value in Source Generation

--- spec/datasource_spec.rb
+++ spec/datasource_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 require 'datasource'
-TOKEN_VALUE = "GltJBUFpINTuqdmrJE0JMNmj6YyWe6_SHmGC579xfLnjoc1aZlN8XV4lJ61zSUwV8L5T4Yhwu6wJ1qQ09X1CuzXU4SWrkOje_83SSgL7Alro9m2J3NdLwe1hqQyi"
+TOKEN_VALUE = "GltJBQv3H8EY0iwgdrglKFwlxvW_MTwXpEeX1_sKLycMffW8jtPI1L-3hqGNwQZ2IvYOizFqYtjUWwYVmA5odK7XjT5urvjyZzfcrCaeY7PRnK0p2XeBI40X9AgQ"
 describe "Datasource" do
     it "should create new data source" do
         @source = Datasource.new
         @source.set_tokey_value(TOKEN_VALUE)
-        expect(@source.create("New name").response.code).to eql(200)
+        expect(@source.create("New name #{rand(1..10000)}").response.code).to eql("200")
     end
 end
 
