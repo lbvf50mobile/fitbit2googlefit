724f0f7329b48642e1e3ae22d1a9da0323e73606
Dmitry <lbvf50.mobile@gmail.com>
2018-01-30 20:58:36 +0700

    Add dataset patch responce-request

--- tmp.rb
+++ tmp.rb
@@ -1,3 +1,7 @@
 require 'open-uri'
-str = "derived:com.google.step_count.delta:407408718192:test:01:03:Created from oAuthPlayground".force_encoding('ASCII-8BIT')
+require 'date'
+str = "derived:com.google.step_count.delta:407408718192:Example Manufacturer:ExampleTablet:1000001:SupperTest22205".force_encoding('ASCII-8BIT')
 puts URI::encode(str)
+p  DateTime.now.strftime('%Q').to_s.length
+p "1397515179728708316".length
+p  DateTime.now.strftime('%Q').to_s + ("0"*6)
